612d287c87e463fb8e05879052086dd83c2b3a3b
Rizal Fakhri <rizal@kodinger.com>
2018-12-25 20:35:16 +0800

    Apply fixes from PHP CS Fixer

--- database/migrations/2018_12_24_045623_add_2fa_activated_and_2fa_provider_column_into_users_table.php
+++ database/migrations/2018_12_24_045623_add_2fa_activated_and_2fa_provider_column_into_users_table.php
@@ -15,7 +15,7 @@ class Add2faActivatedAnd2faProviderColumnIntoUsersTable extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             $table->boolean('2fa_activated')->default(false)->after('password');
-	    $table->string('2fa_provider')->nullable()->after('password');
+            $table->string('2fa_provider')->nullable()->after('password');
         });
     }
 
@@ -28,7 +28,7 @@ class Add2faActivatedAnd2faProviderColumnIntoUsersTable extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             $table->dropColumn('2fa_activated');
-	    $table->dropColumn('2fa_provider');
+            $table->dropColumn('2fa_provider');
         });
     }
 }
